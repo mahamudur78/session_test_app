b352467c5e9ca4ccef088eee29605b224fef8dfc
mahamudur78 <mahamudur78@gmail.com>
2020-12-28 22:59:46 +0600

    Session test 2st project

--- login.php
+++ login.php
@@ -1,2 +1,10 @@
 <?php 
 session_start();
+
+$_SESSION['user'] = $_POST['user'];
+
+if(isset($_SESSION['user'])){
+    echo "Session Created Successfully:- ". $_SESSION['user'];
+}else{
+    echo "No Data Found";
+}
